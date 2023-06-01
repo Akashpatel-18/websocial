68b6a78a2bb4908eeef2c9c301fdfafa7c9e0121
Akash Patel <patelakash1627@gmail.com>
2023-06-01 08:50:47 +0530

    fourth commit

--- backend/index.js
+++ backend/index.js
@@ -5,20 +5,25 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const app = express()
 
+//importing Routes
+const postRoutes = require('./routes/posts')
+const userRoutes = require('./routes/users')
+const { isAuthenticated } = require('./middleware/auth')
+
 //config
 require('dotenv').config()
 mongoose.set('strictQuery', false);
+
+//connnect to database
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("DB Connected"))
+.catch((err) => console.log(err.message))
 
 //middlewares
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
 app.use(cookieParser())
 app.use(cors({origin: process.env.FRONTEND_URL}))
-
-//importing Routes
-const postRoutes = require('./routes/posts')
-const userRoutes = require('./routes/users')
-const { isAuthenticated } = require('./middleware/auth')
 
 //use Routes
 app.use('/posts', postRoutes)
@@ -27,11 +32,6 @@ app.use('/users', userRoutes)
 app.get('/some',isAuthenticated, (req,res) => {
     res.json(req.user)
 })
-
-//connnect to database
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("DB Connected"))
-.catch((err) => console.log(err.message))
 
 //listen to the server
 app.listen(process.env.PORT, () => {
